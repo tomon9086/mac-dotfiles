62653b0d85ec7043c95a6529aa196d86b0537e42
tomon9086 <tomon9086@gmail.com>
2026-03-13 14:50:12 +0900

    refactor: http-serverエイリアスの条件付き定義を削除

--- zsh/alias/common.zsh
+++ zsh/alias/common.zsh
@@ -1,7 +1,3 @@
 alias man="env LANG=ja_JP.UTF-8 man"
 alias jman="env LANG=ja_JP.UTF-8 man"
 alias eman="env LANG=C man"
-
-if command -v http-server 1>/dev/null 2>&1; then
-  alias hs='http-server'
-fi
