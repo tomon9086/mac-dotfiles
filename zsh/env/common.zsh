12c93ffe021d6a1ceeba3134965621b346a2a5a9
tomon9086 <tomon9086@gmail.com>
2026-03-13 09:25:42 +0900

    refactor: PATHの順序を整理し、重複を削除

--- zsh/env/common.zsh
+++ zsh/env/common.zsh
@@ -1,12 +1,14 @@
-export PATH="$HOME/.gem/bin:$PATH"
-export PATH="$HOME/.custom-commands:$PATH"
-export PATH="$HOME/flutter/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
-
-export DENO_INSTALL="$HOME/.deno"
+# PATH
 export PATH="$DENO_INSTALL/bin:$PATH"
-export DVM_DIR="$HOME/.dvm"
 export PATH="$DVM_DIR/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.custom-commands:$PATH"
+export PATH="$HOME/.gem/bin:$PATH"
+export PATH="$HOME/flutter/bin:$PATH"
+
+# tools
+export DENO_INSTALL="$HOME/.deno"
+export DVM_DIR="$HOME/.dvm"
 
 # yarn
 if command -v yarn 1>/dev/null 2>&1; then
